1feee5cd0cde1848b87a154c49ee3859266bb2e4
Janusjia85 <531907642@qq.com>
2022-11-07 18:28:13 +0800

    review

---
+++ src/main/java/com/cyber/infrastructure/filter/AccessGatewayFilter.java
@@ -0,0 +1,41 @@
+package com.cyber.infrastructure.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class AccessGatewayFilter implements GlobalFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AccessGatewayFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String method = request.getMethodValue();
+        String url = request.getPath().value();
+        LOGGER.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
+        //不需要网关签权的url
+        if (authService.ignoreAuthentication(url)) {
+            return chain.filter(exchange);
+        }
+
+        //调用签权服务看用户是否有权限，若有权限进入下一个filter
+        if (permissionService.permission(authentication, url, method)) {
+            ServerHttpRequest.Builder builder = request.mutate();
+            //TODO 转发的请求都加上服务间认证token
+            builder.header(X_CLIENT_TOKEN, "TODO zhoutaoo添加服务间简单认证");
+            //将jwt token中的用户信息传给服务
+            builder.header(X_CLIENT_TOKEN_USER, getUserToken(authentication));
+            return chain.filter(exchange.mutate().request(builder.build()).build());
+        }
+        return unauthorized(exchange);
+    }
+}
